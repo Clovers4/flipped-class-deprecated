0f00e65a86c2da715399e161ede01bf50813351c
w.k <wkaywork@163.com>
2018-11-29 15:53:05 +0800

    [fix] sql文件更新，重新生成实体类和DAO、Mapper文件

--- sql/flipped-class.sql
+++ sql/flipped-class.sql
@@ -16,29 +16,27 @@ PRIMARY KEY (`id`) ,
 UNIQUE INDEX `unique_account` (`account` ASC) USING BTREE COMMENT 'account唯一标识' 
 );
 CREATE TABLE `course` (
-`id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+`teacher_id` int(11) NOT NULL,
 `name` varchar(50) NOT NULL,
 `intro` varchar(255) NULL,
-`need_team` tinyint(5) NOT NULL,
-`share_team` tinyint(5) NOT NULL,
-`share_seminar` tinyint(5) NOT NULL,
-`present_weight` decimal(3,2) NOT NULL,
-`quetion_weight` decimal(3,2) NOT NULL,
+`share_team_master_course_id` tinyint(5) NOT NULL,
+`share_seminar_master_course_id` tinyint(5) NOT NULL,
+`presentation_weight` decimal(3,2) NOT NULL,
 `report_weight` decimal(3,2) NOT NULL,
-`team_rule_id` int(10) UNSIGNED NOT NULL,
+`quetion_weight` decimal(3,2) NOT NULL,
 PRIMARY KEY (`id`) 
 );
 CREATE TABLE `seminar` (
 `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
 `topic` varchar(255) NOT NULL,
 `intro` varchar(255) NULL,
-`visible` tinyint(5) NOT NULL,
-`state` tinyint(5) NOT NULL,
+`state` tinyint(5) NOT NULL COMMENT '状态：不可见、已开放未进行、正在进行、已结束',
+`sign_up_start_time` datetime(6) NOT NULL,
+`sign_up_end_time` datetime(6) NOT NULL,
 `presentation_order_mode` tinyint(5) NOT NULL,
 `team_num_limit` int(10) NOT NULL,
-`report_ddl` datetime(6) NOT NULL ON UPDATE CURRENT_TIMESTAMP(6),
-`sign_up_start_time` datetime(6) NOT NULL ON UPDATE CURRENT_TIMESTAMP(6),
-`sign_up_end_time` datetime(6) NOT NULL ON UPDATE CURRENT_TIMESTAMP(6),
+`report_end_time` datetime(6) NOT NULL ON UPDATE CURRENT_TIMESTAMP(6),
 PRIMARY KEY (`id`) 
 );
 CREATE TABLE `course_seminar` (
@@ -49,6 +47,8 @@ PRIMARY KEY (`id`)
 );
 CREATE TABLE `team_rule` (
 `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+`course_id` int(10) NOT NULL,
+`strategy` tinyint(5) NOT NULL,
 `max_member_num` int(10) NOT NULL,
 `min_member_num` int(10) NOT NULL,
 `end_time` datetime(6) NOT NULL ON UPDATE CURRENT_TIMESTAMP(6),
@@ -58,25 +58,27 @@ CREATE TABLE `share_application` (
 `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
 `master_course_id` int(10) NOT NULL,
 `slave_course_id` int(10) NOT NULL,
-`type` tinyint(5) NOT NULL,
+`function` tinyint(3) NOT NULL COMMENT '表明是共享课程还是共享分组',
+`state` tinyint(3) NOT NULL COMMENT '表明是否已经处理该请求',
+`create_time` datetime(6) NOT NULL,
 PRIMARY KEY (`id`) 
 );
 CREATE TABLE `klass` (
 `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-`name` varchar(50) NOT NULL,
 `course_id` int(10) NOT NULL,
+`name` varchar(50) NOT NULL,
 PRIMARY KEY (`id`) 
 );
-CREATE TABLE `student_klass` (
+CREATE TABLE `klass_student` (
 `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-`user_id` int(10) NOT NULL,
 `klass_id` int(10) NOT NULL,
+`student_id` int(10) NOT NULL,
 PRIMARY KEY (`id`) 
 );
 CREATE TABLE `round` (
 `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-`sigh_up_number` int(10) NULL,
 `course_id` int(10) NOT NULL,
+`max_sigh_up_num` int(10) NULL,
 PRIMARY KEY (`id`) 
 );
 CREATE TABLE `round_seminar` (
@@ -86,23 +88,18 @@ CREATE TABLE `round_seminar` (
 PRIMARY KEY (`id`) 
 );
 CREATE TABLE `presentation` (
-`id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+`course_id` int(11) NULL,
+`seminar_id` int(11) NULL,
 `team_id` int(10) NOT NULL,
+`order` int(5) NULL,
 `score` decimal(3,2) NULL,
-`report_score` decimal(3,2) NULL,
-`seating_order` tinyint(5) NULL,
-`seminard` tinyint(5) NULL,
-`time` datetime(6) NULL ON UPDATE CURRENT_TIMESTAMP(6),
-`material_filename` varchar(50) NULL,
-`material_url` varchar(255) NULL,
-`report_filename` varchar(50) NULL,
-`report_url` varchar(255) NULL,
 PRIMARY KEY (`id`) 
 );
 CREATE TABLE `team` (
 `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-`name` varchar(50) NOT NULL,
 `leader_id` int(10) NOT NULL,
+`name` varchar(50) NOT NULL,
 `valid` tinyint(5) NOT NULL,
 PRIMARY KEY (`id`) 
 );
@@ -114,15 +111,32 @@ PRIMARY KEY (`id`)
 );
 CREATE TABLE `question` (
 `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-`score` decimal(3,2) NULL,
-`student_id` int(10) NOT NULL,
-`team_id` int(10) NOT NULL,
 `presentation_id` int(10) NOT NULL,
+`team_id` int(10) NOT NULL,
+`student_id` int(10) NOT NULL,
+`score` decimal(3,2) NULL,
 PRIMARY KEY (`id`) 
 );
 CREATE TABLE `klass_team` (
 `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-`course_id` int(10) NOT NULL,
+`klass_id` int(10) NOT NULL,
 `team_id` int(11) NOT NULL,
+PRIMARY KEY (`id`) 
+);
+CREATE TABLE `course_share` (
+);
+CREATE TABLE `report` (
+`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+`presentation_id` int(11) NOT NULL,
+`filename` varchar(50) NULL,
+`url` varchar(255) NULL,
+`score` decimal(10,0) NULL,
+PRIMARY KEY (`id`) 
+);
+CREATE TABLE `material` (
+`id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+`presentation_id` int(11) NOT NULL,
+`filename` varchar(50) NULL,
+`url` varchar(255) NULL,
 PRIMARY KEY (`id`) 
 );
